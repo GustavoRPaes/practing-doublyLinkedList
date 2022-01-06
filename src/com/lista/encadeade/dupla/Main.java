5a5bd1382d63a4a335edde6c4677354e1d91fc79
GustavoRPaes <gustavo.rp@aluno.ifsc.edu.br>
2022-01-06 13:56:22 -0300

    Main class implemented, some tests

--- src/com/lista/encadeade/dupla/Main.java
+++ src/com/lista/encadeade/dupla/Main.java
@@ -3,6 +3,25 @@ package com.lista.encadeade.dupla;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        ListaDuplamenteEncadeada<String> minhaListaEncadeada = new ListaDuplamenteEncadeada<>();
+
+        minhaListaEncadeada.add("c1");
+        minhaListaEncadeada.add("c2");
+        minhaListaEncadeada.add("c3");
+        minhaListaEncadeada.add("c4");
+        minhaListaEncadeada.add("c5");
+        minhaListaEncadeada.add("c6");
+        minhaListaEncadeada.add("c7");
+        minhaListaEncadeada.add("c8");
+        System.out.println(minhaListaEncadeada);
+
+        minhaListaEncadeada.remove(3);
+        minhaListaEncadeada.add(3,"99");
+
+        System.out.println(minhaListaEncadeada);
+
+
+
     }
 }
